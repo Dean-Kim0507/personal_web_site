e1d22a000da8ed7eb6d18f738d5be370e57ba73d
Donghyun Kim <dean.kim0507@gmail.com>
2021-05-16 21:46:12 -0600

    commit:)

---
+++ Client/build/precache-manifest.162a47e98556e3c5165577be421d8811.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c30b9afcf732a43c8985950f82c9f52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc9b6cdf4ea691cd67e2",
+    "url": "/static/css/2.0ae79f39.chunk.css"
+  },
+  {
+    "revision": "42164b785c578723cccf",
+    "url": "/static/css/main.efc08762.chunk.css"
+  },
+  {
+    "revision": "cc9b6cdf4ea691cd67e2",
+    "url": "/static/js/2.681f188a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.681f188a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42164b785c578723cccf",
+    "url": "/static/js/main.5f145409.chunk.js"
+  },
+  {
+    "revision": "981c151c4ffdf006847f",
+    "url": "/static/js/runtime-main.13e6e4ce.js"
+  },
+  {
+    "revision": "85663e95be1beac4936dbe582255633f",
+    "url": "/static/media/HomeMyPicture.85663e95.jpg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "27730f288a0cc0704b9eadd098fe1b21",
+    "url": "/static/media/myPicture.27730f28.jpg"
+  }
+]);

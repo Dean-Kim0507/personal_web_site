b970963c05db67a1d3b4b94881c4218f5c996329
Donghyun <dean.kim0507@gmail.com>
2021-07-04 12:44:44 -0600

    ci

--- Client/src/index.js
+++ Client/src/index.js
@@ -7,6 +7,7 @@ import Head from "./Head";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit.min.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css'
 import { Provider } from 'react-redux';
 import store from './store';
 

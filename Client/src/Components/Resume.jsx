af8eab66b0b6cee8d9989e62288f9555d593bd60
Donghyun <dean.kim0507@gmail.com>
2021-07-24 17:53:28 -0600

    resume

--- Client/src/Components/Resume.jsx
+++ Client/src/Components/Resume.jsx
@@ -12,6 +12,7 @@ function Resume() {
 			<h6><li>Location: Calgary / AB / T3K 2Z6</li></h6>
 			<h6><li>Mobile: 403-472-6707</li></h6>
 			<h6><li>E-Mail: dean.kim0507@gmail.com</li></h6>
+			<h6><li>Portfolio: dean-kim.herokuapp.com/</li></h6>
 			<h6><li>LinkedIn: www.linkedin.com/in/donghyunkimdean</li></h6>
 
 			<hr />
@@ -20,10 +21,82 @@ function Resume() {
 				<Card>
 					<Card.Header>
 						<Accordion.Toggle as={Button} variant="link" eventKey="0">
-							Professional Profile
+							Project Experience
 						</Accordion.Toggle>
 					</Card.Header>
 					<Accordion.Collapse eventKey="0">
+						<Card.Body>
+							<Table size="sm" striped bordered>
+								<thead>
+									<tr>
+										<th><h3>Individual Project: Dean’s Canada Shop(Commercial Website)</h3></th>
+									</tr>
+								</thead>
+
+								<tbody>
+									<tr>
+										<th scope="row"><h6>• React, Redux, NodeJS, Express, MongoDB, Redis, Docker, AWS(Elastic Beanstalk, S3)</h6></th>
+									</tr>
+									<tr>
+										<th scope="row"><h6>• CI/CD Pipeline: Github – TravisCI, Docker(build), Jest(test) – AWS(Elastic Beanstalk)</h6></th>
+									</tr>
+									<tr>
+										<th scope="row"><h6>• Server-assisted client side caching: Redis</h6></th>
+									</tr>
+								</tbody>
+							</Table>
+							<Table size="sm" striped bordered>
+								<thead>
+									<tr>
+										<th><h3>Individual Project: My Blog</h3></th>
+									</tr>
+								</thead>
+
+								<tbody>
+									<tr>
+										<th scope="row"><h6>• React, Redux, NodeJS, Express, MySQL, Docker, AWS(Elastic Beanstalk, S3, RDS)</h6></th>
+									</tr>
+									<tr>
+										<th scope="row"><h6>• CI/CD Pipeline: Github – TravisCI, Docker(build), Jest(test) – AWS(Elastic Beanstalk)</h6></th>
+									</tr>
+								</tbody>
+							</Table>
+							<Table size="sm" striped bordered>
+								<thead>
+									<tr>
+										<th><h3>Group Project: FITD Tech(Career Coaching Service)</h3></th>
+									</tr>
+								</thead>
+
+								<tbody>
+									<tr>
+										<th scope="row"><h6>• React, Redux, NodeJS, Express, MongoDB, AWS(EC2, S3)		</h6></th>
+									</tr>
+								</tbody>
+							</Table>
+							<Table size="sm" striped bordered>
+								<thead>
+									<tr>
+										<th><h3>Individual Project: My Portfolio (Interactive Website)</h3></th>
+									</tr>
+								</thead>
+
+								<tbody>
+									<tr>
+										<th scope="row"><h6>• HTML, CSS, React	</h6></th>
+									</tr>
+								</tbody>
+							</Table>
+						</Card.Body>
+					</Accordion.Collapse>
+				</Card>
+				<Card>
+					<Card.Header>
+						<Accordion.Toggle as={Button} variant="link" eventKey="1">
+							Professional Profile
+						</Accordion.Toggle>
+					</Card.Header>
+					<Accordion.Collapse eventKey="1">
 						<Card.Body>
 							<Table size="sm" striped bordered>
 								<thead>
@@ -40,10 +113,25 @@ function Resume() {
 										<th scope="row"><h6>• Object-Oriented Program: Java</h6></th>
 									</tr>
 									<tr>
-										<th scope="row"><h6>• Web programming languages: HTML, CSS, Java Script / React, Redux / NodeJS Express / JSP</h6></th>
+										<th scope="row"><h6>• Web programming languages: HTML, CSS, Java Script / React, Redux / NodeJS, Express / JSP</h6></th>
 									</tr>
 									<tr>
-										<th scope="row"><h6>• Git: Github, AWS, Linux, Database: MongoDB, My SQL, SQL PLUS, PL/SQL.</h6></th>
+										<th scope="row"><h6>• Relational Database: MySQL, SQL PLUS, PL/SQL.</h6></th>
+									</tr>
+									<tr>
+										<th scope="row"><h6>• Document-Oriented Database: MongoDB, In-memory Database: Redis.</h6></th>
+									</tr>
+									<tr>
+										<th scope="row"><h6>• Containerization: Docker, Docker compose.</h6></th>
+									</tr>
+									<tr>
+										<th scope="row"><h6>• CI/CD Environment: TravisCI, Testing: Jest.</h6></th>
+									</tr>
+									<tr>
+										<th scope="row"><h6>• Cloud Computing: AWS(EC2, S3, RDS, Elastic Beanstalk)	.</h6></th>
+									</tr>
+									<tr>
+										<th scope="row"><h6>• Github, Linux, Adobe XD, Power Shell, Networking, Operating system, UX/UI Design		.</h6></th>
 									</tr>
 									<tr>
 										<th scope="row"><h6>• Ability to work self-directed tasks with good time management skills.</h6></th>
@@ -61,11 +149,11 @@ function Resume() {
 				</Card>
 				<Card>
 					<Card.Header>
-						<Accordion.Toggle as={Button} variant="link" eventKey="1">
+						<Accordion.Toggle as={Button} variant="link" eventKey="2">
 							Education
 						</Accordion.Toggle>
 					</Card.Header>
-					<Accordion.Collapse eventKey="1">
+					<Accordion.Collapse eventKey="2">
 						<Card.Body>
 							<Table size="sm" striped bordered>
 								<thead>
@@ -96,11 +184,11 @@ function Resume() {
 				</Card>
 				<Card>
 					<Card.Header>
-						<Accordion.Toggle as={Button} variant="link" eventKey="2">
+						<Accordion.Toggle as={Button} variant="link" eventKey="3">
 							Work Experience
 						</Accordion.Toggle>
 					</Card.Header>
-					<Accordion.Collapse eventKey="2">
+					<Accordion.Collapse eventKey="3">
 						<Card.Body>
 							<Table size="sm" striped bordered>
 								<thead>
@@ -122,7 +210,7 @@ function Resume() {
 												<li>Safety management against maritime disaster.</li>
 												<li>Prevention and response to marine pollution.</li>
 											</h6></th>
-										<th><h6>May 2015 – Aug 2021</h6></th>
+										<th><h6>May 2015 – May 2021</h6></th>
 									</tr>
 									<tr>
 										<th scope="row">Chief Petty Officer / Republic of Korea NAVY
@@ -146,11 +234,11 @@ function Resume() {
 				</Card>
 				<Card>
 					<Card.Header>
-						<Accordion.Toggle as={Button} variant="link" eventKey="3">
+						<Accordion.Toggle as={Button} variant="link" eventKey="4">
 							Certificates
 						</Accordion.Toggle>
 					</Card.Header>
-					<Accordion.Collapse eventKey="3">
+					<Accordion.Collapse eventKey="4">
 						<Card.Body>
 							<Table size="sm" striped bordered>
 								<thead>
